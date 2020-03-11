63e4e54ba902d0f171874e8e99ce36b6ef6b0704
DamasoDaniel <damasodt8a@gmail.com>
2020-03-10 20:35:26 -0600

    Se agregan estilos al body

--- index.view.php
+++ index.view.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Formulario Contacto</title>
-    <link href="https://fonts.googleapis.com/css?family=Roboto&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
     <link rel="stylesheet" href="css/estilos.css">
 </head>
 <body>
