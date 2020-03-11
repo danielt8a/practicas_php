da4078cc077c9470aaabd7dec572ef0e2d0115ea
DamasoDaniel <damasodt8a@gmail.com>
2020-03-10 20:12:24 -0600

    Creando la estructura en html de la práctica del formulario en php

---
+++ index.view.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Formulario Contacto</title>
+    <link href="https://fonts.googleapis.com/css?family=Roboto&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="css/estilos.css">
+</head>
+<body>
+    <div class="wrap">
+        <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>">
+            
+
+        </form>
+    </div>
+</body>
+</html>
