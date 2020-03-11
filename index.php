233ad11f8ac052062a3a6f07b3331c544ddbaed3
DamasoDaniel <damasodt8a@gmail.com>
2020-03-10 21:54:20 -0600

    Comprobación de que el usuario escribió un nombre correctamente

--- index.php
+++ index.php
@@ -4,6 +4,11 @@ if (isset($_POST['submit'])){
     $nombre = $_POST['nombre'];
     $correo = $_POST['correo'];
     $mensaje = $_POST['mensaje'];
+
+    if(!empty($nombre)){
+        $nombre = trim($nombre);
+        $nombre = filter_var($nombre, FILTER_SANITIZE_STRING);
+    }
 }
     require 'index.view.php';
 
